938e3f91970094d587b41775a0d0c2e908051983
Esat <saygina7@gmail.com>
2025-08-21 23:31:44 +0300

    Update exit code logic; align exit command with Bash behavior.

--- src/main.c
+++ src/main.c
@@ -9,7 +9,8 @@
 
 int main() {
     char* command = NULL;
-    size_t size = 0;
+    size_t size;
+    int exit_code = 0;
 
     while (1) {
         printf("shell> ");
@@ -19,29 +20,62 @@ int main() {
             break;
         }
 
+        // trimming spaces at the start and end of the command
         trim(command);
-        if (strcmp(command, "exit") == 0) break;
         if (strlen(command) == 0) continue;
 
+
+        // tokenize
+        char* args[64];
+        int i = 0;
+        char* token = strtok(command, " ");
+        while (token != NULL && i < 63) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+
+
+        // exit command
+        if (strcmp(args[0], "exit") == 0) {
+            if (args[1] != NULL) {
+                char *endptr;
+                long val = strtol(args[1], &endptr, 10);
+
+                if (*endptr == '\0') {
+                    exit_code = (int)(val % 256);
+                    if (exit_code < 0) exit_code += 256;
+                } else {
+                    fprintf(stderr, "exit: The exit code must be a number.\n");
+                    exit_code = 2;
+                    exit(exit_code);
+                }
+
+                if (args[2] != NULL) {
+                    exit_code = 1;
+                    fprintf(stderr, "exit: Too many arguments.\n");
+                    continue;
+                }
+            }
+            exit(exit_code);
+        }
+        
+
+
+        int status;
         pid_t pid = fork();
 
         if (pid > 0) {
-            wait(NULL);
+            wait(&status);
+            if (WIFEXITED(status)) {
+                exit_code = WEXITSTATUS(status);
+            }
         }
         else if (pid == 0) {
-            char* args[64];
-            int i = 0;
-            char* token = strtok(command, " ");
-            while (token != NULL && i < 63) {
-                args[i++] = token;
-                token = strtok(NULL, " ");
-            }
-            args[i] = NULL;
-
             execvp(args[0], args);
 
-            perror("exec failed");
-            _exit(1);
+            perror(args[0]);
+            _exit(127);
         }
         else {
             fprintf(stderr, "Fork failed.\n");
