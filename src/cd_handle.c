08c7803f5c068af72e7077ca04e8f307ea5a7153
Esat <saygina7@gmail.com>
2026-01-24 17:24:02 +0300

    refactor: improve error handling in cd_handle

--- src/cd_handle.c
+++ src/cd_handle.c
@@ -2,56 +2,76 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <errno.h>
 #include <limits.h>
-#include "cd_handle.h"
-
-#ifndef PATH_MAX
-const int PATH_MAX = 4096;
-#endif
 
 
 // cd command
 int cd_handle(char** args, char* cwd, char* last_dir) {
     char* home = getenv("HOME");
     char *new_path = malloc(PATH_MAX);
+
     if (new_path == NULL) {
-        perror("cd: malloc fail");
-        return ENOMEM;
+        fprintf(stderr, "cd: allocation failed");
+        return 1;
     }
+
+    // make it an empty string
     *new_path = '\0';
 
     if (args[1] == NULL) { //if no args
-        strcpy(new_path, home);
-    } else if (args[2] != NULL) { // too many args
-        fprintf(stderr, "Too many arguments for cd.\n");
+        if (home == NULL) {
+            fprintf(stderr, "cd: HOME not set\n");
+            free(new_path);
+            return 1;
+        }
+        strcpy(new_path, home); // set new_path to home dir
+    }
+    else if (args[2] != NULL) { // too many args
+        fprintf(stderr, "cd: too many arguments\n");
         free(new_path);
         return 1;
     }
 
 
-    if (*new_path == '\0' && strcmp(args[1], "-") == 0) { //previous directory
-        strcpy(new_path, last_dir);
+    if (*new_path == '\0' && args[1] && strcmp(args[1], "-") == 0) { //previous directory
+        if (!last_dir || *last_dir == '\0') {
+            fprintf(stderr, "cd: last directory not set");
+            free(new_path);
+            return 1;
+        }
+
+        strcpy(new_path, last_dir); // set new_path to last_dir
         printf("%s\n", new_path);
     }
 
+    // if new_path is still empty set it to first argument
     if (*new_path == '\0')
         strcpy(new_path, args[1]);
-    if (new_path[0] == '~')
+
+    if (new_path[0] == '~') {
+        if (home == NULL) {
+            fprintf(stderr, "cd: HOME not set\n");
+            free(new_path);
+            return 1;
+        }
         snprintf(new_path, PATH_MAX, "%s%s", home, args[1] + 1);
+    }
 
 
     //change dir
-    if (chdir(new_path) == -1) {
+    int ret = chdir(new_path);
+    free(new_path);
+
+    if (ret == -1) {
         perror("cd");
-        free(new_path);
-        return errno;
-    } else {
-        free(new_path);
-        strcpy(last_dir, cwd);
+        return 1;
+    }
+    else {
+        if (last_dir) strcpy(last_dir, cwd);
+
         if (getcwd(cwd, PATH_MAX) == NULL) {
             perror("cd");
-            return errno;
+            return 1;
         }
     }
 
