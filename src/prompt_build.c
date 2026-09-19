f678ac317aa18ad1afba734f674a55379da32f31
Esat <saygina7@gmail.com>
2026-09-19 16:59:56 +0300

    feat: exit code 130 displayed as SIGINT in the prompt

--- src/prompt_build.c
+++ src/prompt_build.c
@@ -6,7 +6,8 @@
 #include "shell.h"
 
 
-void prompt_build(char* prompt, size_t buf_size, const char* home, const char* user) {
+void prompt_build(char* prompt, size_t prompt_size,
+                    const char* home, const char* user) {
     // The path that will appear in the prompt
     char prmpt_cwd[PATH_MAX];
 
@@ -18,9 +19,17 @@ void prompt_build(char* prompt, size_t buf_size, const char* home, const char* u
 
     // Showing the error code in the prompt if it is not 0
     if (shell.exit_code == 0)
-        snprintf(prompt, buf_size, "\033[1;32m%s \033[1;34m%s\033[0m> ",
-                 user, prmpt_cwd);
+        snprintf(prompt, prompt_size,
+                "\033[1;32m%s \033[1;34m%s\033[0m> ",
+                user, prmpt_cwd);
+
+    else if (shell.exit_code == 130)
+        snprintf(prompt, prompt_size,
+                "\033[1;32m%s \033[1;34m%s \033[1;31m[%s]\033[0m> ",
+                user, prmpt_cwd, "SIGINT");
+
     else
-        snprintf(prompt, buf_size, "\033[1;32m%s \033[1;34m%s \033[1;31m[%d]\033[0m> ",
-                 user, prmpt_cwd, shell.exit_code);
+        snprintf(prompt, prompt_size,
+                "\033[1;32m%s \033[1;34m%s \033[1;31m[%d]\033[0m> ",
+                user, prmpt_cwd, shell.exit_code);
 }
